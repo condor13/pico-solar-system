29a3aedfa6f83f116524371691191097ab17a315
Teodor Nicolae <theonickolas@yahoo.com>
2023-04-01 12:55:24 +0300

    Add files via upload

---
+++ pico solar system/main.py
@@ -0,0 +1,238 @@
+from picographics import PicoGraphics, DISPLAY_PICO_DISPLAY, PEN_RGB565
+from pimoroni import Button, RGBLED
+from PiicoDev_LIS3DH import PiicoDev_LIS3DH
+from PiicoDev_Unified import sleep_ms # cross-platform compatible sleep function
+
+import ds3231
+import time
+import math
+import gc
+import machine
+from my_lib import RTC_DS3231
+from micropython import const
+
+motion = PiicoDev_LIS3DH()
+motion.set_tap(1, threshold=40) # set up single-tap detection with a tap threshold of 40
+
+gc.enable()
+backlight = 0.7
+plusDays = 0
+change = 0
+penis = 0 #variable for timeout backlight
+denis = 1 #variable for backlight
+
+display = PicoGraphics(display=DISPLAY_PICO_DISPLAY, rotate=0, pen_type=PEN_RGB565)
+button_a = Button(12)
+button_b = Button(13)
+button_x = Button(14)
+button_y = Button(15)
+led = RGBLED(6, 7, 8)
+led.set_rgb(0,0,0)
+
+
+
+def circle(xpos0, ypos0, rad):
+    x = rad - 1
+    y = 0
+    dx = 1
+    dy = 1
+    err = dx - (rad << 1)
+    while x >= y:
+        display.pixel(xpos0 + x, ypos0 + y)
+        display.pixel(xpos0 + y, ypos0 + x)
+        display.pixel(xpos0 - y, ypos0 + x)
+        display.pixel(xpos0 - x, ypos0 + y)
+        display.pixel(xpos0 - x, ypos0 - y)
+        display.pixel(xpos0 - y, ypos0 - x)
+        display.pixel(xpos0 + y, ypos0 - x)
+        display.pixel(xpos0 + x, ypos0 - y)
+        if err <= 0:
+            y += 1
+            err += dy
+            dy += 2
+        if err > 0:
+            x -= 1
+            dx += 2
+            err += dx - (rad << 1)
+
+
+def check_for_buttons():
+    global denis
+    global backlight
+    global plusDays
+    global change
+    
+    if motion.tapped:
+        print("Tapped")
+        
+    if button_x.is_pressed:
+        denis = 1
+    elif button_y.is_pressed:
+        denis = 1
+    if button_a.is_pressed and button_b.is_pressed:
+        plusDays = 0
+        change = 2
+        time.sleep(0.2)
+    elif button_a.is_pressed:
+        plusDays += 86400
+        change = 3
+        time.sleep(0.05)
+    elif button_b.is_pressed:
+        plusDays -= 86400
+        change = 3
+        time.sleep(0.05)
+
+
+def set_internal_time(utc_time):
+    rtc_base_mem = const(0x4005c000)
+    atomic_bitmask_set = const(0x2000)
+    (year, month, day, hour, minute, second, wday, yday) = time.localtime(utc_time)
+    machine.mem32[rtc_base_mem + 4] = (year << 12) | (month << 8) | day
+    machine.mem32[rtc_base_mem + 8] = ((hour << 16) | (minute << 8) | second) | (((wday + 1) % 7) << 24)
+    machine.mem32[rtc_base_mem + atomic_bitmask_set + 0xc] = 0x10
+ 
+
+def main():
+    global change
+    import planets
+    from pluto import Pluto
+    #set_time()
+
+    def draw_planets(HEIGHT, ti):
+        PL_CENTER = (68, int(HEIGHT / 2))
+        planets_dict = planets.coordinates(ti[0], ti[1], ti[2], ti[3], ti[4])
+        # t = time.ticks_ms()
+        display.set_pen(display.create_pen(255, 255, 0))
+        display.circle(int(PL_CENTER[0]), int(PL_CENTER[1]), 4)
+        for i, el in enumerate(planets_dict):
+            r = 8 * (i + 1) + 2
+            display.set_pen(display.create_pen(40, 40, 40))
+            circle(PL_CENTER[0], PL_CENTER[1], r)
+            feta = math.atan2(el[0], el[1])
+            coordinates = (r * math.sin(feta), r * math.cos(feta))
+            coordinates = (coordinates[0] + PL_CENTER[0], HEIGHT - (coordinates[1] + PL_CENTER[1]))
+            for ar in range(0, len(planets.planets_a[i][0]), 5):
+                x = planets.planets_a[i][0][ar] - 50 + coordinates[0]
+                y = planets.planets_a[i][0][ar + 1] - 50 + coordinates[1]
+                if x >= 0 and y >= 0:
+                    display.set_pen(display.create_pen(planets.planets_a[i][0][ar + 2], planets.planets_a[i][0][ar + 3],
+                                    planets.planets_a[i][0][ar + 4]))
+                    display.pixel(int(x), int(y))
+        # print("draw = " + str(time.ticks_diff(t, time.ticks_ms())))
+
+    w = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    m = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    display.set_pen(display.create_pen(0, 0, 0))
+    display.clear()
+    display.update()
+    
+    gc.collect()
+
+    HEIGHT = const(135)
+
+    mi = -1
+    pl = Pluto(display)
+
+    seconds_absolute = time.time()
+    ti = time.localtime(seconds_absolute + plusDays)
+    da = ti[2]
+
+    draw_planets(HEIGHT, ti)
+    start_int = time.ticks_ms()
+    while True:
+        global backlight
+        global penis
+        global denis
+        ticks_dif = time.ticks_diff(time.ticks_ms(), start_int)
+        if ticks_dif >= 1000 or time.time() != seconds_absolute:
+            seconds_absolute = time.time()
+            ti = time.localtime(seconds_absolute + plusDays)
+            start_int = time.ticks_ms()
+            ticks_dif = 0
+        if change > 0:
+            ti = time.localtime(seconds_absolute + plusDays)
+        if da != ti[2]:
+            da = ti[2]
+            change = 3
+
+        if change > 0:
+            if change == 1:
+                display.set_pen(display.create_pen(0, 0, 0))
+                display.clear()
+                draw_planets(HEIGHT, ti)
+                if plusDays > 0:
+                    led.set_rgb(0, 50, 0)
+                elif plusDays < 0:
+                    led.set_rgb(50, 0, 0)
+                else:
+                    led.set_rgb(0, 0, 0)
+                change = 0
+            else:
+                change -= 1
+
+        display.set_pen(display.create_pen(0, 0, 0))
+        display.rectangle(140, 0, 100, HEIGHT)
+        display.rectangle(130, 0, 110, 35)
+        display.rectangle(130, 93, 110, HEIGHT - 93)
+
+        if mi != ti[4]:
+            mi = ti[4]
+            pl.reset()
+        pl.step(ti[5], ticks_dif)
+        pl.draw()
+
+        display.set_pen(display.create_pen(244, 170, 30))
+        display.text("%02d %s %d " % (ti[2], m[ti[1] - 1], ti[0]), 132, 7, 70, 2)
+        display.set_pen(display.create_pen(65, 129, 50))
+        display.text(w[ti[6]], 135, 93, 99, 2)
+        display.set_pen(display.create_pen(130, 255, 100))
+        display.text("%02d:%02d" % (ti[3], ti[4]), 132, 105, 99, 4)
+        
+        if denis == 1:     #display is on
+            penis += 1
+            display.set_backlight(0.7)
+        if penis == 300:
+            denis = 0      #display is off
+            penis = 0
+            display.set_backlight(0)
+
+        display.update()
+        check_for_buttons()
+        time.sleep(0.01)
+        
+        #print(denis)
+        #print(penis)
+
+
+def set_time():
+        import ds3231
+        ds = ds3231.ds3231()
+        set_internal_time(ds.read_time())
+
+
+def set_time_ntp(wifi_config):
+    import network
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    print("Connecting to:", wifi_config.ssid)
+    wlan.connect(wifi_config.ssid, wifi_config.key)
+    while not wlan.isconnected() and wlan.status() >= 0:
+        print("Waiting for connection...")
+        time.sleep(5)
+    print(wlan.ifconfig())
+    print("Pico clock:", time.localtime())
+    print("Setting time via ntp...")
+    import ntptime
+    ntpsuccess = False
+    while not ntpsuccess:
+        try:
+            ntptime.settime()
+            print("Time set: ", time.localtime())
+            ntpsuccess = True
+        except:
+            print("NTP failure. Retrying.")
+            time.sleep(5)
+
+
+time.sleep(0.5)
+main()
